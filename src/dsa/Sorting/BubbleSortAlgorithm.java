ac861552900dc8c9a6bad7d8ed46efeaab0bbe6c
doguhannilt <doguhannilt@gmail.com>
2025-03-11 10:01:43 +0300

    feat(sorting):bubble algorithm added

---
+++ src/dsa/Sorting/BubbleSortAlgorithm.java
@@ -0,0 +1,27 @@
+package dsa;
+
+import java.util.Arrays;
+
+public class BubbleSortAlgorithm {
+
+    public static void main(String[] args) {
+        int[] arr = {1,5,2,5,4};
+        System.out.println(Arrays.toString(arr));
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void bubble(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 1; j < arr.length - i; j++) {
+                if (arr[j] < arr[j - 1]) {
+                    // Swap
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                }
+            }
+        }
+    }
+
+}
